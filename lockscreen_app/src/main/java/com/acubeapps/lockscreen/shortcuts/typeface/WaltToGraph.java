3166910cf5199866a28da9c4896ff455d86dd224
Aasha Medhi <aasha.medhi@inmobi.com>
2016-07-14 14:02:20 +0530

    Added font

---
+++ lockscreen_app/src/main/java/com/acubeapps/lockscreen/shortcuts/typeface/WaltToGraph.java
@@ -0,0 +1,25 @@
+package com.acubeapps.lockscreen.shortcuts.typeface;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+/**
+ * Created by badrinath.kulkarni on 11/24/15.
+ */
+public class WaltToGraph {
+    private static Typeface mFont = null;
+
+    private static Typeface getFontTypeface(Context context) {
+        if (mFont == null)
+            mFont = Typeface.createFromAsset(context.getAssets(), "fonts/waltographUI.ttf");
+        return mFont;
+    }
+
+    public static TextView applyFont(Context context, TextView textView) {
+        if (textView != null)
+            textView.setTypeface(getFontTypeface(context));
+        return textView;
+    }
+}
+
